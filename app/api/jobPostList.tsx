ccd405cbe62e5f8de8512b2d3d3cd379b9eba661
Rory <mitty0304@naver.com>
2023-11-08 06:02:40 +0900

    feat(api): add graphql api

--- app/api/jobPostList.tsx
+++ app/api/jobPostList.tsx
@@ -5,8 +5,20 @@ import {
   GetJobPostDocument,
   GetJobPostQuery,
   GetJobPostQueryVariables,
+  GetJobPostByFilterDocument,
+  GetJobPostByFilterQueryVariables,
+  GetJobPostByFilterQuery,
+  GetHighlightedJobPostDocument,
+  GetHighlightedJobPostQuery,
+  GetHighlightedJobPostQueryVariables,
+  JobCategory as JobCategoryType,
 } from "../graphql/generated";
 
+export { JobCategoryType };
+
+//
+// Get job post by id
+//
 interface getJobPostByIdParams {
   uuid: string;
 }
@@ -25,4 +37,70 @@ export const getJobPostById = async ({ uuid }: getJobPostByIdParams) => {
 
   const jobPost = data.jobPostCollection?.edges;
   return { jobPost };
+};
+
+//
+// Get job post by filter options (category, area)
+//
+interface GetJobPostByFilterProps {
+  jobCategory: JobCategoryType[];
+  siDo?: string[];
+  first: number;
+  after?: string | undefined | null;
+}
+
+export const getJobPostByFilter = async ({
+  jobCategory,
+  siDo,
+  after,
+  first,
+}: GetJobPostByFilterProps) => {
+  const { data } = await client.query<
+    GetJobPostByFilterQuery,
+    GetJobPostByFilterQueryVariables
+  >({
+    query: GetJobPostByFilterDocument,
+    variables: {
+      jobCategory,
+      siDo,
+      first,
+      ...(!!after && { after }),
+    },
+    fetchPolicy: getFetchPolicy(),
+  });
+
+  const jobPost = data.jobPostCollection?.edges;
+  const pageInfo = data.jobPostCollection?.pageInfo;
+
+  return { jobPost, pageInfo };
+};
+
+//
+// Get highlighted post
+//
+interface GetHighlightedJobPostProps {
+  first: number;
+  after?: string | undefined | null;
+}
+
+export const getHighlightedJobPost = async ({
+  after,
+  first,
+}: GetHighlightedJobPostProps) => {
+  const { data } = await client.query<
+    GetHighlightedJobPostQuery,
+    GetHighlightedJobPostQueryVariables
+  >({
+    query: GetHighlightedJobPostDocument,
+    variables: {
+      first,
+      ...(!!after && { after }),
+    },
+    fetchPolicy: getFetchPolicy(),
+  });
+
+  const jobPostList = data.jobPostCollection?.edges;
+  const pageInfo = data.jobPostCollection?.pageInfo;
+
+  return { jobPostList, pageInfo };
 };
