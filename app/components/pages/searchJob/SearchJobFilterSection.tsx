b82f4ec150992ee98a153949dc502ca190999dbd
Rory <mitty0304@naver.com>
2023-11-08 06:11:09 +0900

    feat(SearchJobFilterSection): filter job list and add area search bottom sheet

--- app/components/pages/searchJob/SearchJobFilterSection.tsx
+++ app/components/pages/searchJob/SearchJobFilterSection.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, FlatList, Pressable, TouchableOpacity } from "react-native";
+import {
+  ScrollView,
+  View,
+  FlatList,
+  Pressable,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import SearchJobPostListItem from "./SearchJobPostListItem";
 import ArrowPathIcon from "react-native-heroicons/solid/ArrowPathIcon";
 import MapPinIcon from "react-native-heroicons/solid/MapPinIcon";
@@ -7,9 +14,18 @@ import MapPinIcon from "react-native-heroicons/solid/MapPinIcon";
 import Stack from "app/components/blocks/Stack/Stack";
 import Chip from "app/components/blocks/Chip/Chip";
 import SearchJobAreaBox from "./SearchJobAreaBox";
+import {
+  BottomSheetScrollView,
+  BottomSheetBackdrop,
+  BottomSheetModal,
+} from "@gorhom/bottom-sheet";
 
+import { useQuery } from "@tanstack/react-query";
+import { getJobPostByFilter, JobCategoryType } from "app/api/jobPostList";
 import { JobType, JobTypeCategory } from "app/constants/JobCategory";
+import { BottomSheetDefaultBackdropProps } from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types";
 
+const JOB_POST_LIST_COUNT = 5;
 interface JobPostListItemProps {
   id: number;
   title: string;
@@ -21,60 +37,142 @@ interface JobPostListItemProps {
 }
 
 const SearchJobFilterSection: React.FC = () => {
-  const snapPoints = React.useMemo(() => ["80%"], []);
-
-  const [selectedJobType, setSelectedJobType] = React.useState<string>(
-    JobType.All
-  );
+  const [selectedJobType, setSelectedJobType] = React.useState<
+    keyof typeof JobType
+  >(JobType.All);
   const [selectedArea, setSelectedArea] = React.useState<string[]>([]);
-  const [searchResult, setSearchResult] = React.useState<
-    JobPostListItemProps[]
-  >([]);
+  const [cursor, setCursor] = React.useState<string | undefined | null>();
+
+  // Bottom Sheet
+  const bottomSheetRef = React.useRef<BottomSheetModal>(null);
+  const snapPoints = React.useMemo(() => ["65%"], []);
+
+  const { data, isError, error } = useQuery({
+    queryKey: [selectedArea, selectedJobType],
+    queryFn: async () =>
+      await getJobPostByFilter({
+        jobCategory: [JobCategoryType.Etc],
+        siDo: ["경기도"],
+        after: cursor,
+        first: JOB_POST_LIST_COUNT,
+      }),
+  });
+
+  const searchedJobPost = data?.jobPost;
+  const pageInfo = data?.pageInfo;
+
+  React.useEffect(() => {
+    setCursor(pageInfo?.endCursor);
+  }, [pageInfo]);
 
   const handleOptionResetClick = () => {
     setSelectedJobType(JobType.All);
     setSelectedArea([]);
   };
 
-  React.useEffect(() => {
-    setSearchResult([]);
-  }, [selectedJobType, selectedArea]);
+  const handleAreaFilterOpen = () => {
+    bottomSheetRef.current?.present();
+  };
+
+  const handleBottomSheetClose = () => {
+    bottomSheetRef.current?.close();
+  };
+
+  const renderBackdrop = React.useCallback(
+    (
+      props: React.JSX.IntrinsicAttributes & BottomSheetDefaultBackdropProps
+    ) => (
+      <BottomSheetBackdrop
+        {...props}
+        disappearsOnIndex={1}
+        appearsOnIndex={2}
+        pressBehavior={"close"}
+      />
+    ),
+    []
+  );
+
+  const ResetButton = (
+    <Pressable onPress={handleOptionResetClick}>
+      <ArrowPathIcon />
+    </Pressable>
+  );
+
+  const AreaFilter = (
+    <TouchableOpacity onPress={handleAreaFilterOpen}>
+      <Stack styles="items-center border-b px-1 pb-0.5 justify-between">
+        <Text className="mr-1">
+          {selectedArea.length === 0
+            ? "지역"
+            : selectedArea.length === 1
+            ? selectedArea[0]
+            : `${selectedArea[0]} 외 ${selectedArea.length - 1} 곳`}
+        </Text>
+        <MapPinIcon width={20} height={20} />
+      </Stack>
+    </TouchableOpacity>
+  );
+
+  const JobTypeFilter = (
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <Stack columnGap={8} styles="mx-4">
+        {(Object.keys(JobTypeCategory) as Array<keyof typeof JobType>).map(
+          (jobTypekey) => (
+            <Chip
+              key={jobTypekey}
+              label={JobTypeCategory[jobTypekey]}
+              variant={jobTypekey === "All" ? "filled" : "outlined"}
+              active={jobTypekey === selectedJobType}
+              onPress={() => setSelectedJobType(jobTypekey)}
+            />
+          )
+        )}
+      </Stack>
+    </ScrollView>
+  );
 
   return (
-    <View className='bg-white'>
-      <View className="py-2">
-        <Stack styles="justify-between mb-2">
-          <>
-            <Pressable onPress={handleOptionResetClick}>
-              <ArrowPathIcon />
-            </Pressable>
-            <TouchableOpacity>
-              <MapPinIcon />
-            </TouchableOpacity>
-          </>
+    <View className="py-4 bg-white">
+      <View>
+        <Stack styles="justify-between mb-6 px-4">
+          {ResetButton}
+          {AreaFilter}
         </Stack>
-        <Stack>
-          {(Object.keys(JobTypeCategory) as Array<keyof typeof JobType>).map(
-            (jobTypekey) => (
-              <Chip
-                key={jobTypekey}
-                label={JobTypeCategory[jobTypekey]}
-                variant={jobTypekey === "All" ? "filled" : "outlined"}
-                active={jobTypekey === selectedJobType}
-                onPress={() => setSelectedArea([jobTypekey])}
-              />
-            )
-          )}
-        </Stack>
+        {JobTypeFilter}
       </View>
       <View>
         <FlatList
           scrollEnabled={false}
-          data={searchResult}
-          renderItem={({ item }) => <SearchJobPostListItem {...item} />}
-          keyExtractor={(item) => `${item.id}`}
+          data={searchedJobPost}
+          renderItem={({ item }) => (
+            <SearchJobPostListItem {...item.node} isBookMarked={false} />
+          )}
+          keyExtractor={(item) => `${item.node.id}`}
+          ListEmptyComponent={
+            <Stack styles="justify-center items-center h-80 ">
+              <Text className="text-base text-neutral-500">
+                게제된 공고가 아직 없어요
+              </Text>
+            </Stack>
+          }
         />
       </View>
+      <BottomSheetModal
+        ref={bottomSheetRef}
+        index={0}
+        snapPoints={snapPoints}
+        backdropComponent={renderBackdrop}
+      >
+        <BottomSheetScrollView>
+          <SearchJobAreaBox
+            defaultArea={selectedArea}
+            onSaveBtnClick={(areaList) => {
+              setSelectedArea(areaList);
+              handleBottomSheetClose();
+            }}
+          />
+        </BottomSheetScrollView>
+      </BottomSheetModal>
     </View>
   );
 };
