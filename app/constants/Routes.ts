38a8333714ffe0e753e9ae71fdcda09d6dd3a002
Rory <mitty0304@naver.com>
2023-11-14 17:13:41 +0900

    feat(Routes): add my page and resume page routes

--- app/constants/Routes.ts
+++ app/constants/Routes.ts
@@ -2,12 +2,25 @@ export enum NAV_SCREENS {
   HomeScreen = "Home",
   SearchJobScreen = "SearchJob",
   JobPostDetailScreen = "JobPostDetail",
-  // visa
+
+  // Visa
   VisaScreen = "Visa",
   VisaTestD101 = "VisaTestD101",
   VisaTestF27 = "VisaTestF27",
-  // my page
+
+  // MyPage
   MyPageScreen = "MyPage",
+  MyApplyListScreen = "MyApplyListScreen",
+  MyVisaHistoryScreen = "MyVisaHistoryScreen",
+  MyVisaEnrollScreen = "MyVisaEnrollScreen",
+
+  // Resume
+  MyResumeScreen = "MyResumeScreen",
+  ResumeEditBasicInfoScreen = "ResumeEditBasicInfoScreen",
+  ResumeEditEduCareerScreen = "ResumeEditEduCareerScreen",
+  ResumeEditLanguageScreen = "ResumeEditLanguageScreen",
+
+  // Notice Page
   NoticeScreen = "NoticePage",
   NoticeItemScreen = "NoticeItemScreen",
   MyNotificationScreen = "MyNotificationScreen",
