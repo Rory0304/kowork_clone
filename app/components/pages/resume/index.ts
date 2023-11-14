033c1b7f5f816edaa4d9739dd21325317665af37
Rory <mitty0304@naver.com>
2023-11-14 17:27:11 +0900

    feat(Resume): add sections used in resume page

---
+++ app/components/pages/resume/index.ts
@@ -0,0 +1,11 @@
+export { default as ProgressStep } from "./ProgressStep";
+
+// Step1: Basic Info
+export { default as BasicInfoFormSection } from "./BasicInfoFormSection";
+export { default as ResidenceFormSection } from "./ResidenceFormSection";
+export { default as EmailFormSection } from "./EmailFormSection";
+
+// Step2: Education and Career
+export { default as EducationFormSection } from "./EducationFormSection";
+export { default as CareerFormSection } from "./CareerFormSection";
+export { default as LanguageFormSection } from "./LanguageFormSection";
