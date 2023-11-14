0b404a593e7685851d5a31c9d03bd26ad9b00e90
Rory <mitty0304@naver.com>
2023-11-14 17:09:25 +0900

    feat(Form): add resume form parts

---
+++ app/components/pages/resume/Form/CountrySelectBox.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  ScrollView,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { countries } from "app/constants/Country.json";
+import TextInput from "./TextInput";
+
+interface CountrySelectBoxProps {
+  onPress: (value: string) => void;
+}
+
+const CountrySelectBox: React.FC<CountrySelectBoxProps> = ({ onPress }) => {
+  const [keyword, setKeyword] = React.useState("");
+  const [searchedCountries, setSearchedCountries] = React.useState(countries);
+
+  React.useEffect(() => {
+    const result = searchedCountries.filter(
+      (country) =>
+        country.ko.includes(keyword) || country.name.includes(keyword)
+    );
+
+    setSearchedCountries(result);
+  }, [keyword]);
+
+  return (
+    <FlatList
+      data={searchedCountries}
+      keyExtractor={(item) => item.name}
+      ListHeaderComponent={
+        <View className="py-2 bg-white">
+          <Text className="pb-2 text-neutral-500">
+            You can type only korean or english
+          </Text>
+          <TextInput
+            value={keyword}
+            placeholder="국가 검색"
+            onChangeText={(value) => setKeyword(value)}
+          />
+        </View>
+      }
+      renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => onPress(`${item.name}(${item.ko})`)}>
+          <Text className="py-4 font-medium">
+            {item.name}({item.ko})
+          </Text>
+        </TouchableOpacity>
+      )}
+      stickyHeaderIndices={[0]}
+      className="px-4"
+    />
+  );
+};
+
+export default CountrySelectBox;
---
+++ app/components/pages/resume/Form/TextInput.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { TextInput as RnTextInput } from "react-native";
+import type { TextInputProps as RnTextInputProps } from "react-native";
+import { gray } from "tailwindcss/colors";
+
+interface TextInputProps extends RnTextInputProps {
+  error?: boolean;
+}
+
+const TextInput: React.FC<TextInputProps> = ({ error, ...props }) => {
+  return (
+    <RnTextInput
+      {...props}
+      focusable
+      className={`p-3 text-base font-medium bg-slate-100 border border-gray-300 rounded-lg ${
+        error ? "border-red-500" : "focus:border-primary"
+      }`}
+      placeholderTextColor={gray[400]}
+    />
+  );
+};
+
+export default TextInput;
