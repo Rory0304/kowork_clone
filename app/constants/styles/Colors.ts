2b699e61d9a9711e5cdceff2f1d0f6948d6fc494
Rory <mitty0304@naver.com>
2023-11-17 02:02:50 +0900

    chore(styles); add custom colors constants and move global syltes directory

---
+++ app/constants/styles/Colors.ts
@@ -0,0 +1,5 @@
+export const customColors = {
+  primary: "#2c5ae9",
+  secondary: "#878d91",
+  danger: "#dc4c64",
+};
--- app/constants/styles/Global.ts
+++ app/constants/styles/Global.ts
@@ -1,4 +1,4 @@
-const contentLayoutStyle = "px-4 pb-12";
+const contentLayoutStyle = "px-4 pt-4 pb-12";
 
 const roundedBox = "border rounded-md";
 const grayBoxStyle = "border border-gray-200 rounded-md bg-gray-50";
