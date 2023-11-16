7dcf995590840cca9dc4e4700e021121db6dc5f3
Rory <mitty0304@naver.com>
2023-11-17 02:25:55 +0900

    fix(VisaAdBanner): fix image position

--- app/components/pages/visa/VisaAdBanner.tsx
+++ app/components/pages/visa/VisaAdBanner.tsx
@@ -1,28 +1,24 @@
 import React from "react";
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, Image } from "react-native";
 
 const VisaAdBanner: React.FC = () => {
   return (
-    <View
-      className="flex flex-row px-4 py-5 rounded-md bg-primary"
-      style={{
-        flexDirection: "row",
-      }}
-    >
+    <View className="relative flex flex-row flex-1 px-4 py-5 rounded-lg bg-primary">
       <View>
-        <Text className="text-6xl font-bold text-white ">VISA</Text>
-        <Text className="text-base text-white">{`비자 정보 찾느라 더이상 헤매지 마세요.\n간편하게 확인해보세요!`}</Text>
+        <Text className="text-5xl font-bold text-white">VISA</Text>
+        <Text className="text-sm text-white">{`비자 정보 찾느라 더이상 헤매지 마세요.\n간편하게 확인해보세요!`}</Text>
       </View>
-      <Image
-        alt=""
-        source={require("../../../../assets/images/zoom.png")}
-        resizeMode="contain"
-        style={{
-          width: "100%",
-          height: null,
-          resizeMode: "contain",
-        }}
-      />
+      <View className="absolute top-4 w-[40%] h-full right-1">
+        <Image
+          alt=""
+          source={require("../../../../assets/images/zoom.png")}
+          resizeMode="contain"
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </View>
     </View>
   );
 };
