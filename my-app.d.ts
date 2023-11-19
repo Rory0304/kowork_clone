96202d52801f350ab7ac838bbe5f08f76d6df264
Rory <mitty0304@naver.com>
2023-11-19 20:23:57 +0900

    chore(my-app.d.ts): add svg module

--- my-app.d.ts
+++ my-app.d.ts
@@ -1,2 +1,11 @@
 // ref: https://github.com/marklawlor/nativewind/issues/77
 /// <reference types="nativewind/types" />
+
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module "*.css";
