63f3fbf46d4435d28e492ad0c3d214ba22a954e4
Rory <mitty0304@naver.com>
2023-11-19 20:28:50 +0900

    feat(supabase): add supabase client util

---
+++ app/utils/supabase.ts
@@ -0,0 +1,10 @@
+// ref: https://github.com/supabase/supabase/issues/8464#issuecomment-1221448428
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseClient = createClient(
+  process.env.SUPABASE_STORE_URL!,
+  process.env.SUPABASE_API_KEY!
+);
+
+export { supabaseClient };
