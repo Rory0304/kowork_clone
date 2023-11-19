41e01aa2512dfee2b2a5b78408e9dcc3270cf1ed
Rory <mitty0304@naver.com>
2023-11-19 20:29:10 +0900

    feat: add Home screen

--- app/screens/Home/HomeScreen.tsx
+++ app/screens/Home/HomeScreen.tsx
@@ -1,32 +1,59 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
 import {
-  VisaRecommendSection,
-  VisaTestSection,
-  JobPostListSection,
-  ImportantNoticeSection,
-  HomeNoticeBanner,
-  SearchJobMainBanner,
-} from "../../components/pages";
-import VStack from "app/components/blocks/Stack/VStack";
-import { contentLayoutStyle } from "app/constants/styles/Global";
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import navigate from "app/utils/navigationHelper";
+
+import { Stack } from "app/components/blocks";
+import LogoIcon from "assets/images/logo.svg";
 
 const HomeScreen: React.FC = () => {
+  const navigation = useNavigation();
+  const navigator = navigate(navigation);
+
   return (
-    <ScrollView>
-      <HomeNoticeBanner />
-      <ImportantNoticeSection />
-      <SearchJobMainBanner />
-      <JobPostListSection />
-      <View className="px-4">
-        <VStack space={8}>
-          <VisaRecommendSection />
-        </VStack>
-        <VStack space={8}>
-          <VisaTestSection description="쉽고 빠르게 비자 취득 요건을 확인해보세요!" />
-        </VStack>
-      </View>
-    </ScrollView>
+    <View className="relative flex justify-between flex-1 border-red-100 brder">
+      <ImageBackground
+        source={require("../../../assets/images/home-background.jpg")}
+        resizeMode="cover"
+        style={{
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Stack direction="column" styles="justify-center px-4 h-full">
+          <Stack
+            direction="column"
+            styles="justify-center items-center basis-3/4"
+          >
+            <View className="mb-8">
+              <Text className="mb-2 text-base font-medium text-white">
+                외국인 구인구직 전문 플랫폼
+              </Text>
+              <LogoIcon width={175} height={25} fill="#fff" />
+            </View>
+            <Text className="text-2xl text-center text-white">
+              {"Korean-based Recruitment \n& Visa Consulting"}
+            </Text>
+          </Stack>
+          <Stack direction="column" styles="basis-1/4 ">
+            <TouchableOpacity onPress={() => navigator.openEmailSignInScreen()}>
+              <View className="px-2 py-4 bg-white rounded-xl">
+                <Text className="text-base font-bold text-center">
+                  이메일로 계속하기
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </Stack>
+        </Stack>
+      </ImageBackground>
+    </View>
   );
 };
 
