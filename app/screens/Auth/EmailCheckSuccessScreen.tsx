5480acda7f4b0f3eec8c7f34bf29d2a57c8167ae
Rory <mitty0304@naver.com>
2023-12-05 21:20:27 +0900

    feat(EmailCheckSuccessScreen): add email check success screen

--- app/screens/Auth/EmailCheckSuccessScreen.tsx
+++ app/screens/Auth/EmailCheckSuccessScreen.tsx
@@ -1,18 +1,40 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { Button, Stack } from "app/components/blocks";
+import CheckCircleIcon from "react-native-heroicons/solid/CheckCircleIcon";
+import { customColors } from "app/constants/styles/Colors";
+import { useNavigation } from "@react-navigation/native";
+import navigate from "app/utils/navigationHelper";
 
 const EmailCheckSuccessScreen: React.FC = () => {
-  console.log("here");
+  const navigation = useNavigation();
+  const navigator = navigate(navigation);
 
   return (
     <Stack direction="column" styles=" justify-center flex-1 px-4 pb-12">
-      <View className="mb-2">
-        <Text className="text-sm font-medium text-center text-neutral-500">
-          성공적으로 회원가입되었습니다!
+      <View className="mb-4">
+        <Stack direction="row" styles="items-center justify-center mb-6">
+          <Text className="mr-2 text-2xl font-bold text-center text-primary">
+            회원가입 완료
+          </Text>
+          <CheckCircleIcon
+            color={customColors.primary}
+            width={32}
+            height={32}
+          />
+          <Stack />
+        </Stack>
+        <Text className="text-base font-medium text-center text-neutral-500">
+          {`이메일 인증이 완료되었어요!\n이제 프로필 설정을 마무리해주세요.`}
         </Text>
       </View>
-      <Button label="다음" variant="filled" color="primary" />
+      <Button
+        isFullWidth
+        label="다음"
+        variant="filled"
+        color="primary"
+        onPress={() => navigator.openProfileEnrollUserTypeScreen()}
+      />
     </Stack>
   );
 };
