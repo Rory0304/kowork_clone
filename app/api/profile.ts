3491ba1d16636541b49df5c6a9add0a0612e5772
Rory <mitty0304@naver.com>
2023-12-05 21:06:03 +0900

    feat(profile): add profile api

---
+++ app/api/profile.ts
@@ -0,0 +1,50 @@
+import client from "../graphql/client";
+import { getFetchPolicy } from "../utils/getFetchPolicy";
+
+import {
+  GetProfileDocument,
+  GetProfileQuery,
+  GetProfileQueryVariables,
+} from "../graphql/generated";
+import { supabaseClient } from "app/utils/supabase";
+import type { ProfileType } from "app/types/Profile";
+
+interface GetProfileByUserIdProps {
+  userId: string;
+}
+
+export const getProfileByUserId = async ({
+  userId,
+}: GetProfileByUserIdProps) => {
+  const { data } = await client.query<
+    GetProfileQuery,
+    GetProfileQueryVariables
+  >({
+    query: GetProfileDocument,
+    variables: {
+      userId: userId,
+    },
+    fetchPolicy: getFetchPolicy(),
+  });
+
+  console.log("data", data);
+  const profile = data.profileCollection?.edges?.[0]?.node || null;
+
+  return { profile };
+};
+
+interface InsertProfileDataProps extends ProfileType {
+  userId: string;
+}
+
+export const insertProfileData = async (props: InsertProfileDataProps) => {
+  const { data, error } = await supabaseClient
+    .from("Profile")
+    .insert({ ...props });
+
+  if (error) {
+    throw new Error("fail to insert profile data");
+  }
+
+  return data;
+};
