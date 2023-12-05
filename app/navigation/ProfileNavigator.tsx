4cdafd062d42ae47905391917a1ea9732f7cdb03
Rory <mitty0304@naver.com>
2023-12-05 21:17:32 +0900

    feat(ProfileNavigator): fix conditional profile navigate

--- app/navigation/ProfileNavigator.tsx
+++ app/navigation/ProfileNavigator.tsx
@@ -1,26 +1,57 @@
 import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NAV_SCREENS } from "../constants/Routes";
-import EmailCheckSuccessScreen from "app/screens/Auth/EmailCheckSuccessScreen";
+import ProfileEnrollUserTypeScreen from "app/screens/Profile/ProfileEnrollUserTypeScreen";
+import ProfileEnrollBasicInfoScreen from "app/screens/Profile/ProfileEnrollBasicInfoScreen";
+import ProfileEnrollVisaInfoScreen from "app/screens/Profile/ProfileEnrollVisaInfoScreen";
+import { ProfileFormType } from "app/types/Profile";
+import { profileSchemaResolver } from "app/constants/validation/Profile";
 
 export type NativeStackParamList = {
-  EmailCheckSuccessScreen: undefined;
+  ProfileEnrollUserTypeScreen: undefined;
+  ProfileEnrollBasicInfoScreen: undefined;
+  ProfileEnrollVisaInfoScreen: undefined;
 };
 
 const NativeStack = createNativeStackNavigator<NativeStackParamList>();
 
 const ProfileNavigator: React.FC = () => {
+  const method = useForm<ProfileFormType>({
+    defaultValues: {},
+    resolver: profileSchemaResolver,
+  });
+
   return (
-    <NativeStack.Navigator>
-      <NativeStack.Screen
-        name={NAV_SCREENS.EmailCheckSuccessScreen}
-        component={EmailCheckSuccessScreen}
-        options={{
-          headerBackVisible: true,
-          headerTitle: "회원가입",
-        }}
-      />
-    </NativeStack.Navigator>
+    <FormProvider {...method}>
+      <NativeStack.Navigator>
+        <NativeStack.Screen
+          name={NAV_SCREENS.ProfileEnrollUserTypeScreen}
+          component={ProfileEnrollUserTypeScreen}
+          options={{
+            presentation: "fullScreenModal",
+            headerBackVisible: true,
+            headerTitle: "회원 구분",
+          }}
+        />
+        <NativeStack.Screen
+          name={NAV_SCREENS.ProfileEnrollBasicInfoScreen}
+          component={ProfileEnrollBasicInfoScreen}
+          options={{
+            headerBackVisible: true,
+            headerTitle: "프로필 설정",
+          }}
+        />
+        <NativeStack.Screen
+          name={NAV_SCREENS.ProfileEnrollVisaInfoScreen}
+          component={ProfileEnrollVisaInfoScreen}
+          options={{
+            headerBackVisible: true,
+            headerTitle: "비자 정보입력",
+          }}
+        />
+      </NativeStack.Navigator>
+    </FormProvider>
   );
 };
 
