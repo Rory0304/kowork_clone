de255b27e1100da6d3d874315625e6ca1c7f30b1
Rory <mitty0304@naver.com>
2023-12-05 21:38:43 +0900

    chore: export form components

--- app/components/blocks/Form/index.ts
+++ app/components/blocks/Form/index.ts
@@ -1,3 +1,4 @@
-export {default as FormInputBox} from './FormInputBox';
-export {default as CheckBox} from './CheckBox';
-export {default as TextInput} from './TextInput';
+export { default as FormInputBox } from "./FormInputBox";
+export { default as CheckBox } from "./CheckBox";
+export { default as TextInput } from "./TextInput";
+export { default as TextDropDownInput } from "./TextDropDownInput";
