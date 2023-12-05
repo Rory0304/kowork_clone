265f596a9c14dd311f4d22a8249c80805c6f1462
Rory <mitty0304@naver.com>
2023-12-05 21:05:26 +0900

    feat(FormInputBox): add required adornment

--- app/components/blocks/Form/FormInputBox.tsx
+++ app/components/blocks/Form/FormInputBox.tsx
@@ -4,15 +4,22 @@ import { View, Text } from "react-native";
 interface FormInputBoxProps {
   title: string;
   InputComponent: React.ReactElement;
+  required?: boolean;
 }
 
 const FormInputBox: React.FC<FormInputBoxProps> = ({
   title,
   InputComponent,
+  required = false,
 }) => {
   return (
     <View className="py-4">
-      <Text className="mb-4 text-base font-bold">{title}</Text>
+      <View className="flex flex-row items-center mb-4">
+        <Text className="text-base font-bold">{title}</Text>
+        {required ? (
+          <Text className="text-base font-bold text-primary">*</Text>
+        ) : null}
+      </View>
       <View>{InputComponent}</View>
     </View>
   );
