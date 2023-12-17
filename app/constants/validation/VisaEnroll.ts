2d53127d3204bcedf494842cc2c5ea6150f32fd8
Rory0304 <mitty0304@naver.com>
2023-12-17 23:48:24 +0900

    feat(VisaEnroll): add required validation message

--- app/constants/validation/VisaEnroll.ts
+++ app/constants/validation/VisaEnroll.ts
@@ -6,13 +6,14 @@ const VISA_ENROLL_FORM_SCHEMA = yup.object().shape({
   visaStatus: yup.string().oneOf(Object.values(VisaCode)).required("체류 자격을 선택해주세요"),
   visaIssueDate: yup
     .string()
-    .required()
+    .required('필수 입력 사항입니다.')
     .checkIsFullDateFormat("올바르지 않은 날짜 형식입니다."),
   visaFinalEntryDate: yup
     .string()
-    .required()
-    .checkIsFullDateFormat("올바르지 않은 날짜 형식입니다."),
-});
+    .required('필수 입력 사항입니다.')
+    .checkIsFullDateFormat("올바르지 않은 날짜 형식입니다.")
+    .checkIsDateAfterThreshold(yup.ref('visaIssueDate'), '입국 만료일은 발급일 이후여야 합니다.')
+  })
 
 export const visaEnrollSchemaResolver = yupResolver(
     VISA_ENROLL_FORM_SCHEMA
