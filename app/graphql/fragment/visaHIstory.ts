ea892eaf6c1db98aacd2048c12235a6f497233ce
Rory0304 <mitty0304@naver.com>
2023-12-17 16:01:12 +0900

    feat(visaHistory): add id props to visa history graphql

--- app/graphql/fragment/visaHIstory.ts
+++ app/graphql/fragment/visaHIstory.ts
@@ -2,6 +2,7 @@ import { gql } from "@apollo/client";
 
 export const FRAGMENT_VISA_HISTORY = gql`
   fragment VisaHistoryFields on VisaHistory {
+    id
     visaStatus
     visaIssueDate
     visaFinalEntryDate
