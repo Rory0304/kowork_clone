691ad62097786171423b68f9800225dbbdcaa0ed
Rory0304 <mitty0304@naver.com>
2023-12-22 22:54:26 +0900

    feat(resume): add resume layout components

---
+++ app/components/pages/resume/layout/ResumeFormWrapperTitle.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+interface ResumeFormWrapperTitleProps {
+  title: string;
+  step: number;
+}
+
+const ResumeFormWrapperTitle: React.FC<ResumeFormWrapperTitleProps> = ({
+  title,
+  step,
+}) => {
+  return (
+    <View className="p-4 py-6 bg-white">
+      <Text className="mb-1 text-lg font-bold text-neutral-300">
+        {step < 10 ? `0${step}` : step}
+      </Text>
+      <Text className="text-2xl font-bold text-neutral-500">{title}</Text>
+    </View>
+  );
+};
+
+export default ResumeFormWrapperTitle;
