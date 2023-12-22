9ded703713bf545f54584e2de37f6b0e5cd82403
Rory0304 <mitty0304@naver.com>
2023-12-22 23:05:01 +0900

    style(Button): change button specifications

--- app/components/blocks/Button/Button.tsx
+++ app/components/blocks/Button/Button.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native";
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  TouchableWithoutFeedbackProps,
+} from "react-native";
 import Stack from "app/components/blocks/Stack/Stack";
 import clsx from "clsx";
 
 type ButtonVariantType = "default" | "filled" | "dashed" | "outlined";
 type ButtonColor = "default" | "primary";
-type ButtonSize = "default" | "large";
+type ButtonSize = "default" | "large" | "small";
 
-interface ButtonProps {
+interface ButtonProps extends TouchableWithoutFeedbackProps {
   label: string;
   variant?: ButtonVariantType;
   isFullWidth?: boolean;
   color?: ButtonColor;
-  disabeld?: boolean;
   size?: ButtonSize;
   Icon?: React.ReactNode;
-  onPress?: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -24,34 +27,37 @@ const Button: React.FC<ButtonProps> = ({
   color = "default",
   isFullWidth = false,
   size = "default",
-  disabeld = false,
   Icon,
-  onPress,
+  ...props
 }) => {
   const ButtonWrapperStyles = clsx(
     // disable
-    disabeld === true ? "opacity-30" : "opactiy-100",
+    props.disabled === true ? "opacity-30" : "opactiy-100",
     `justify-center items-center rounded-lg ${isFullWidth ? "w-full" : ""}`,
     // variant
     variant === "dashed" &&
       color === "primary" &&
       "border border-primary border-dashed",
+    variant === "filled" && "bg-neutral-100",
     variant === "filled" && color === "primary" && "bg-primary",
     variant === "default" && "bg-neutral-100",
+    variant === "outlined" && "border border-neutral-100",
+    variant === "outlined" && color === "primary" && "border border-primary",
     // size
     size === "default" && "px-4 py-3",
-    size === "large" && "px-4 py-4"
+    size === "large" && "px-4 py-4",
+    size === "small" && "px-2 py-2"
   );
 
   const ButtonLabelStyles = clsx(
     "text-base font-bold text-center",
     color === "default" && "text-neutral-400",
-    variant === "filled" && color === "primary" && "text-white",
-    variant === "dashed" && color === "primary" && "text-primary"
+    color === "primary" && "text-primary",
+    variant === "filled" && color === "primary" && "text-white"
   );
 
   return (
-    <TouchableOpacity onPress={onPress} disabled={disabeld}>
+    <TouchableOpacity {...props}>
       <Stack styles={ButtonWrapperStyles}>
         {Icon ? <View className="mr-2">{Icon}</View> : null}
         <Text className={ButtonLabelStyles}>{label}</Text>
