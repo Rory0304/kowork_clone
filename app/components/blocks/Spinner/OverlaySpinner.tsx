922f7339c07878c4b012933a91b33da7f5939e6c
Rory0304 <mitty0304@naver.com>
2023-12-24 18:11:18 +0900

    style(OverlaySpinner): add some styles of overlay spinner

--- app/components/blocks/Spinner/OverlaySpinner.tsx
+++ app/components/blocks/Spinner/OverlaySpinner.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { View } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
+import { customColors } from "app/constants/styles/Colors";
 
 const OverlaySpinner: React.FC = () => {
   return (
-    <View className="absolute top-0 left-0 flex flex-1 w-full h-full">
-      <ActivityIndicator size="large" />
+    <View className="bg-gray-300 opacity-80 absolute top-0 left-0 justify-center items-center flex flex-1 w-full h-full z-10">
+      <ActivityIndicator size="large" color={customColors.primary} />
     </View>
   );
 };
