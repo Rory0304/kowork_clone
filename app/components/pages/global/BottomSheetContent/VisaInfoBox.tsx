1f54e77b89bfffabb276973787e8b9f628425d6f
Rory0304 <mitty0304@naver.com>
2023-12-26 22:27:36 +0900

    feat(VisaInfoBox): add visa info box bottom sheet component

---
+++ app/components/pages/global/BottomSheetContent/VisaInfoBox.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import CheckCircleIcon from 'react-native-heroicons/solid/CheckCircleIcon';
+import DocumentTextIcon from 'react-native-heroicons/solid/DocumentTextIcon';
+import StarIcon from 'react-native-heroicons/solid/StarIcon';
+import UserCircleIcon from 'react-native-heroicons/solid/UserCircleIcon';
+
+import { Stack } from 'app/components/blocks';
+import { BottomSheetCloseBtn } from 'app/components/blocks/BottomSheet/BottomSheet';
+import { customColors } from 'app/constants/styles/Colors';
+
+interface VisaInfoBoxHeaderProps {
+  title: string;
+  onClose: () => void;
+}
+
+interface VisaInfoBoxContentProps {
+  description: string;
+  subject: string;
+  document: string;
+}
+
+export const VisaInfoBoxHeader: React.FC<VisaInfoBoxHeaderProps> = ({
+  title,
+  onClose,
+}) => {
+  return (
+    <React.Fragment>
+      <BottomSheetCloseBtn onClose={onClose} />
+      <Text className="text-base font-bold">{title}</Text>
+      <StarIcon fill={customColors.primary} />
+    </React.Fragment>
+  );
+};
+
+const VisaInfoBottomSheetContent = ({
+  Icon,
+  title,
+  content,
+}: {
+  Icon: React.ReactNode;
+  title: string;
+  content: string;
+}) => {
+  return (
+    <View className="bg-gray-50 rounded-lg p-4">
+      <View className="pb-2 mb-4 flex flex-row items-center border-b border-gray-300">
+        {Icon}
+        <Text className="ml-2 font-bold text-neutral-500 text-base">
+          {title}
+        </Text>
+      </View>
+      <View>
+        <Text className="text-neutral-500 text-base">{content}</Text>
+      </View>
+    </View>
+  );
+};
+
+export const VisaInfoBoxContent: React.FC<VisaInfoBoxContentProps> = ({
+  description,
+  subject,
+  document,
+}) => {
+  return (
+    <ScrollView>
+      <Stack direction="column" rowGap={8} styles="p-4">
+        <VisaInfoBottomSheetContent
+          Icon={<DocumentTextIcon fill={customColors.neutral[500]} />}
+          title="설명"
+          content={description}
+        />
+        <VisaInfoBottomSheetContent
+          Icon={<UserCircleIcon fill={customColors.neutral[500]} />}
+          title="대상자"
+          content={subject}
+        />
+        <VisaInfoBottomSheetContent
+          Icon={<CheckCircleIcon fill={customColors.neutral[500]} />}
+          title="필요서류"
+          content={document}
+        />
+      </Stack>
+    </ScrollView>
+  );
+};
