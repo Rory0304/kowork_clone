0085cc8c91ec7c62fac6a019f373aa4a4ad8f31e
Rory0304 <mitty0304@naver.com>
2023-12-29 03:24:44 +0900

    feat(MyPageTabNavigator): add bookmark button to header

--- app/navigation/MyPageTabNavigator.tsx
+++ app/navigation/MyPageTabNavigator.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import StarIcon from 'react-native-heroicons/outline/StarIcon';
 
+import { useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import MyPageScreen from 'app/screens/MyPage/MyPageScreen';
+import navigate from 'app/utils/navigationHelper';
+
 import { NAV_SCREENS } from '../constants/Routes';
-import MyPageScreen from '../screens/MyPage/MyPageScreen';
 
 const NativeStack = createNativeStackNavigator();
 
 const MyPageNavigator: React.FC = () => {
+  const navigation = useNavigation();
+  const navigator = navigate(navigation);
+
   return (
     <NativeStack.Navigator initialRouteName={NAV_SCREENS.MyPageScreen}>
       <NativeStack.Screen
@@ -15,6 +23,11 @@ const MyPageNavigator: React.FC = () => {
         component={MyPageScreen}
         options={{
           headerTitle: '내 정보',
+          headerRight: () => (
+            <TouchableOpacity onPress={() => navigator.openMyBookmarkScreen()}>
+              <StarIcon />
+            </TouchableOpacity>
+          ),
         }}
       />
     </NativeStack.Navigator>
