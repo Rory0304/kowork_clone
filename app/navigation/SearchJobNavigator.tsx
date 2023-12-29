98d726b6da280259b0854122293f8bfc68b77ae0
Rory0304 <mitty0304@naver.com>
2023-12-30 02:00:59 +0900

    feat(SearchJobNavigator): navigate to search job post screen

--- app/navigation/SearchJobNavigator.tsx
+++ app/navigation/SearchJobNavigator.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import MagnifyingGlassIcon from 'react-native-heroicons/outline/MagnifyingGlassIcon';
 
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HeaderBackIcon from 'app/components/blocks/Icon/HeaderBackIcon';
 import { NAV_SCREENS } from 'app/constants/Routes';
+import navigate from 'app/utils/navigationHelper';
 
-import SearchJobScreen from '../screens/SearchJob/SearchJobScreen';
+import JobPostScreen from '../screens/JobPost/JobPostScreen';
 
 const NativeStack = createStackNavigator();
 
 const SearchJobNavigator: React.FC = () => {
+  const navigation = useNavigation();
+  const navigator = navigate(navigation);
   return (
     <NativeStack.Navigator>
       <NativeStack.Screen
         name={NAV_SCREENS.SearchJobScreen}
-        component={SearchJobScreen}
+        component={JobPostScreen}
         options={{
           headerTitleStyle: { display: 'none' },
           headerLeft: () => (
             <Text className="text-xl font-bold">일자리 찾기</Text>
           ),
-          headerRight: () => <MagnifyingGlassIcon color="text-neutral-400" />,
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigator.openSearchJobPostScreen()}
+            >
+              <MagnifyingGlassIcon color="text-neutral-400" />
+            </TouchableOpacity>
+          ),
         }}
       />
     </NativeStack.Navigator>
